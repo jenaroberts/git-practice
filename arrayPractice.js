8190c7e7fce6c81e13efb9272e7c1b97f1a224e6
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-04-20 16:15:26 -0400

    arrayobject practice

--- arrayPractice.js
+++ arrayPractice.js
@@ -2,17 +2,18 @@ const fruits = ['apple', 'banana', 'orange', 'plum']
 const groceries = ['milk', 'fish', 'beer']
 const newArray = fruits.concat(groceries)
 
+
 fruits.push('mango')
 fruits.unshift('kiwi')
 
-function removeItems(){
-fruits.shift()
-fruits.pop()
-console.log(fruits)
-}
-removeItems()
+// function removeItems(){
+// fruits.shift()
+// fruits.pop()
+// console.log(fruits)
+// }
+// removeItems()
 
-delete fruits[2]
+// delete fruits[2]
 
 console.log(fruits)
 
@@ -20,3 +21,4 @@ console.log(newArray)
 
 console.log(newArray.sort())
 console.log(newArray.reverse())
+
