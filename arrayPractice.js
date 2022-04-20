317dba386cf4d93babf9b373c51263efa2e41392
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-04-20 17:51:19 -0400

    arrayobject practice

--- arrayPractice.js
+++ arrayPractice.js
@@ -22,3 +22,18 @@ console.log(newArray)
 console.log(newArray.sort())
 console.log(newArray.reverse())
 
+
+let indexOfApp = fruits.indexOf('apple')
+
+console.log(indexOfApp)
+
+
+const arrayInput = [1, 2, 3, 4, 5, 6, 7]
+// const target = 3
+
+function arrTest(arr, target){
+    return arr.indexOf(target) !== -1
+    }
+    // console.log('Did we find the culprit ' + arrTest(arrayInput, 4567))
+    let check = 0.1 + 0.2 == 0.3
+    console.log(check)
