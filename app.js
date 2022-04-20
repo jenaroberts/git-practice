c82cad0bf46a0e3640c5e085faa1bfa7c1a77940
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-04-20 14:09:57 -0400

    app more consolelogs

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('test')
+
+
+function addNumbers (num1, num2){
+    return num1 + num2
+}
+
+addNumbers(4, 6)
+console.log(addNumbers(4, 6))
+console.log(addNumbers(7, 6))
+console.log(addNumbers(9, 6))
