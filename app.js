78cadeaf2bc2897e7e4a71ad85b4c5283f40dc00
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-04-20 16:05:20 -0400

    arrayobject practice

--- app.js
+++ app.js
@@ -10,3 +10,24 @@ console.log(addNumbers(4, 6))
 console.log(addNumbers(7, 6))
 console.log(addNumbers(9, 6))
 console.log('Hello')
+
+
+// define the function
+// define the number - 27
+// cube it 
+// call function
+
+
+function cubeNumber(num1){
+    res = num1 * num1 * num1
+    return res
+}
+console.log(cubeNumber(3))
+
+const jonsFamily = {
+    wife:'Claudia',
+    d1: ' mia',
+    d2: 'annalee',
+    pet1: '🐶',
+}
+console.log(jonsFamily.d1)
---
+++ arrayPractice.js
@@ -0,0 +1,22 @@
+const fruits = ['apple', 'banana', 'orange', 'plum']
+const groceries = ['milk', 'fish', 'beer']
+const newArray = fruits.concat(groceries)
+
+fruits.push('mango')
+fruits.unshift('kiwi')
+
+function removeItems(){
+fruits.shift()
+fruits.pop()
+console.log(fruits)
+}
+removeItems()
+
+delete fruits[2]
+
+console.log(fruits)
+
+console.log(newArray)
+
+console.log(newArray.sort())
+console.log(newArray.reverse())
